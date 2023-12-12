dc6f304581e0f7954d338a604607c887094400a8
Bruno Reis <reisfmb@gmail.com>
2023-12-12 22:01:30 +0100

    main.ts #19

--- src/main/resources/main.ts
+++ src/main/resources/main.ts
@@ -1,27 +1,76 @@
-import { submitTask } from '/lib/xp/task';
-import { importedFunction } from './lib/vanilla';
+import * as projectLib from '/lib/xp/project';
+import * as contextLib from '/lib/xp/context';
+import * as clusterLib from '/lib/xp/cluster';
+import * as exportLib from '/lib/xp/export';
 import {DEBUG_MODE} from '/constants'; // Using relative, so it will be inlined (and tree-shaken).
 
-DEBUG_MODE && log.info('Hello from transpiled TS server-side code in main.js.');
-
-// Test that deconstruction works after transpilation.
-const obj = { prop: 'Deconstruction works :)' };
-const { prop } = obj;
-DEBUG_MODE && log.info(prop);
-
-const submitTasks = () => {
-	const taskConfig = {
-		descriptor: 'task',
-		name: `${app.name}:task:nameA`
-	};
-
-	// Submit (lib.task.submitTask) two named tasks with identical names to showcase how to intercept duplicate task runs
-	submitTask(taskConfig);
-	submitTask(taskConfig);
+const projectData = {
+    id: 'myfirstsite',
+    displayName: 'My First Site',
+    description: 'Intro to building websites with the Enonic framework',
+    readAccess: {
+        public: true
+    }
 }
 
-// Call an imported function
-importedFunction();
+function runInContext(callback) {
+    let result;
+    try {
+        result = contextLib.run({
+            principals: ["role:system.admin"],
+            repository: 'com.enonic.cms.' + projectData.id,
+        }, callback);
+    } catch (e) {
+        DEBUG_MODE && log.info('Error: ' + e.message);
+    }
 
-// Showcase Task API
-submitTasks();
+    return result;
+}
+
+function createProject() {
+    return projectLib.create(projectData);
+}
+
+function getProject() {
+    return projectLib.get({ id: projectData.id });
+}
+
+function initializeProject() {
+    let project = runInContext(getProject);
+
+    if (!project) {
+        DEBUG_MODE && log.info('Project "' + projectData.id + '" not found. Creating...');
+        project = runInContext(createProject);
+
+        if (project) {
+            DEBUG_MODE && log.info('Project "' + projectData.id + '" successfully created');
+            DEBUG_MODE && log.info('Importing "' + projectData.id + '" data');
+
+            runInContext(createContent);
+        } else {
+            DEBUG_MODE && log.error('Project "' + projectData.id + '" failed to import');
+        }
+    }
+}
+
+function createContent() {
+    const importNodes = exportLib.importNodes({
+        source: resolve('/import'),
+		targetNodePath: '/content',
+		xslt: resolve('/import/replace_app.xsl'),
+		xsltParams: { applicationId: app.name },
+        includeNodeIds: true,
+        nodeImported: () => {},
+        nodeResolved: () => {}
+    });
+
+    if (importNodes.importErrors.length > 0) {
+        DEBUG_MODE && log.warning('Errors:');
+        importNodes.importErrors.forEach(element => DEBUG_MODE && log.warning(element.message));
+    }
+}
+
+if (clusterLib.isMaster()) {
+    initializeProject();
+}
+
