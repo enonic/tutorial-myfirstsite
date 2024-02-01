596014aa710d8e2bc3bda2d3f1a6f0a5de725bec
alansemenov <ase@enonic.com>
2024-02-01 12:56:52 +0100

    Docs for handling static assets (#37)

---
+++ code-samples/pages/hello-static/hello.ts
@@ -0,0 +1,30 @@
+import type { Response } from '/index.d';
+
+// @ts-expect-error no-types
+import { render } from '/lib/thymeleaf';
+import { getContent } from '/lib/xp/portal';
+import { getAssetUrl } from '../../static';
+
+// Specify the view file to use
+const VIEW = resolve('./hello.html');
+
+// Handle the GET request
+export function get(): Response {
+
+	// Get the content that is using the page
+	const content = getContent();
+
+	// Prepare the model that will be passed to the view
+	const model = {
+		displayName: content.displayName,
+		mainRegion: content.page.regions.main,
+		staticCssUrl: getAssetUrl('index.css'),
+	}
+
+	// Prepare the response object
+	const response: Response = {
+		body: render(VIEW, model)
+	};
+
+	return response;
+}
