cb91e0e87301c915b513ee37853f42fac28eb7be
alansemenov <ase@enonic.com>
2024-02-01 12:56:00 +0100

    Added support for lib-static (#37)

--- src/main/resources/site/static.ts
+++ src/main/resources/site/static.ts
@@ -3,9 +3,60 @@ import type {
 	Response,
 } from '/index.d';
 
+import {
+	getSite,
+	pageUrl as getPageUrl,
+} from '/lib/xp/portal';
 // @ts-expect-error TS2307: Cannot find module '/lib/enonic/static' or its corresponding type declarations.
 import {buildGetter} from '/lib/enonic/static';
-import { GETTER_ROOT } from '/constants';
+import ioResource from '../lib/ioResource';
+import {IS_DEV_MODE} from '../lib/runMode';
+
+import {
+	FILEPATH_MANIFEST_CJS,
+	FILEPATH_MANIFEST_ESM,
+	GETTER_ROOT,
+} from '/constants';
+
+type UrlPostfixParams = {
+	manifestPath?: string
+	path: string,
+};
+
+type UrlParams = UrlPostfixParams & {urlPrefix: string};
+
+const manifests = {
+	[FILEPATH_MANIFEST_CJS]: ioResource(FILEPATH_MANIFEST_CJS),
+	[FILEPATH_MANIFEST_ESM]: ioResource(FILEPATH_MANIFEST_ESM)
+}
+
+const getImmutableUrl = ({
+	manifestPath = FILEPATH_MANIFEST_ESM,
+	path,
+	urlPrefix
+}: UrlParams) => {
+	if (IS_DEV_MODE) {
+		manifests[manifestPath] = ioResource(manifestPath);
+	}
+
+	return `${urlPrefix}/${GETTER_ROOT}/${manifests[manifestPath][path]}`;
+}
+
+export const getAssetUrl = (path: string) => {
+	const sitePath = getSite()._path;
+	let urlPrefix = getPageUrl({
+		path: sitePath
+	});
+	if (urlPrefix === '/') {
+		urlPrefix = '';
+	}
+
+	return getImmutableUrl({
+		manifestPath: FILEPATH_MANIFEST_CJS,
+		path,
+		urlPrefix
+	});
+}
 
 const staticGetter = buildGetter({
 	etag: false, // default is true in production and false in development
@@ -23,7 +74,6 @@ const staticGetter = buildGetter({
 		if (r !== GETTER_ROOT) {
 			throw new Error(`Path outside root! ${request.rawPath}`);
 		}
-
 		return `/${rawPathParts.join('/')}`;
 	},
 	root: GETTER_ROOT,
